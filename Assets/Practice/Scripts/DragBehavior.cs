3b50684e34b791b6c02bca8e7482366533ad48ff
chakabaka1027 <alexanderdcha@gmail.com>
2017-02-16 23:54:24 -0500

    mirrors are now moveable via grabbing from a distance

---
+++ Assets/Practice/Scripts/DragBehavior.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragBehavior : MonoBehaviour {
+
+    public GameObject rightPalm;
+    public GameObject openHandCursor;
+    public GameObject closedHandCursor;
+    public LayerMask rotatable;
+
+    GameObject mirror;
+
+    private float sensitivity;
+     private Vector3 palmPosReference;
+     private Vector3 palmOffset;
+     private Vector3 rotation;
+     public bool isRotating;
+
+	// Use this for initialization
+	void Start () {
+		sensitivity = 200;
+        rotation = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        IdentifyRotationObj();
+	    if(isRotating && mirror != null) {
+            closedHandCursor.SetActive(true);
+
+            //offset
+            palmOffset = (rightPalm.transform.position - palmPosReference);
+
+            //apply rotation
+            rotation.y = -(palmOffset.x + palmOffset.y) * sensitivity;
+
+            //rotate
+            mirror.transform.Rotate(rotation);
+
+            //store palm
+            palmPosReference = rightPalm.transform.position;
+        } else {
+            closedHandCursor.SetActive(false);
+        }
+
+
+	}
+
+    private void IdentifyRotationObj() {
+        Vector3 ray = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
+		RaycastHit hit;
+
+        if(Physics.Raycast(ray, Camera.main.transform.forward, out hit, Mathf.Infinity, rotatable)) {
+            mirror = hit.collider.gameObject;
+            openHandCursor.SetActive(true);
+            
+
+        } else {
+            openHandCursor.SetActive(false);
+            if (!isRotating) {
+                mirror = null;
+
+            }
+        }
+    }
+
+
+
+    public void RotateMirrorLeftRight() {
+        isRotating = true;
+
+        palmPosReference = rightPalm.transform.position;
+    }
+
+    public void StopRotatingLeftRight() {
+        isRotating = false;
+    }
+
+}
--- Assets/Practice/Scripts/Movement.cs
+++ Assets/Practice/Scripts/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
     public GameObject cursor;
+    public GameObject movementCursor;
     GameObject nextNavpoint;
     GameObject currentNavpoint;
 
@@ -30,19 +31,21 @@ public class Movement : MonoBehaviour {
         Vector3 ray = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
 		RaycastHit hit;
 
-        if(Physics.Raycast(ray, Camera.main.transform.forward, out hit, Mathf.Infinity, moveable) && canMove) {
+        if(Physics.Raycast(ray, Camera.main.transform.forward, out hit, Mathf.Infinity, moveable)) {
             nextNavpoint = hit.collider.gameObject;
-            
+            movementCursor.SetActive(true);
             //uncomment if you want to move instantly when looking at desired navpoint
             //MoveToNavPoint();
 
         } else {
             nextNavpoint = null;
+            movementCursor.SetActive(false);
+
         }
     }
 
     public void MoveToNavPoint() {
-        if(canMove && nextNavpoint!= null) {
+        if(canMove && nextNavpoint!= null && FindObjectOfType<DragBehavior>().isRotating == false) {
             if (currentNavpoint != null) {
                 currentNavpoint.SetActive(true);
             }
@@ -50,22 +53,22 @@ public class Movement : MonoBehaviour {
             gameObject.transform.position = nextNavpoint.transform.position;
             nextNavpoint.SetActive(false);
             currentNavpoint = nextNavpoint;
-            cursor.SetActive(false);
+            movementCursor.SetActive(false);
         }
     }
 
     public void EngageMovementMode() {
-        if(!canMove) {
-            cursor.SetActive(true);
-            canMove = true;
-        }
+        cursor.SetActive(true);
+        canMove = true;
     }
 
     public void DisengageMovementMode() {
-        if (canMove) {
-            cursor.SetActive(false);
-            canMove = false;
-        }
+        cursor.SetActive(false);
+    }
+
+    public IEnumerator RemoveCursor() {
+        yield return new WaitForSeconds(0.01f);
+        canMove = false;
         
         
         
