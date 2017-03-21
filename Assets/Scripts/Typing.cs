0e915a5fe9922ede9127bc50fa77780421069317
chakabaka1027 <alexanderdcha@gmail.com>
2017-03-21 01:57:17 -0400

    ui canvas for rotation added

---
+++ Assets/Scripts/Typing.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Text))]
+public class Typing : MonoBehaviour {
+
+	public string msg = "Replace";
+	private Text textComp;
+	public float startDelay = 2f;
+	public float typeDelay = 0.01f;
+	public AudioClip putt;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("TypeIn");
+	}
+
+	void Awake(){
+		textComp = GetComponent<Text>();
+	}
+
+	public IEnumerator TypeIn(){
+		yield return new WaitForSeconds(startDelay);
+		for (int i = 0; i < msg.Length; i++){
+			textComp.text = msg.Substring(0, i);
+			GetComponent<AudioSource>().PlayOneShot(putt, 0.03f);
+			yield return new WaitForSeconds(typeDelay);
+		}
+	}
+
+	public IEnumerator TypeOff(){
+		for(int i =msg.Length; i >= 0; i--){
+			textComp.text = msg.Substring(0, i);
+			yield return new WaitForSeconds(typeDelay);
+		}
+	}
+}
