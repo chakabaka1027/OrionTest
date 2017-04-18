2847e1fbd21feddbcf672c888d9b82d1925a2cdd
chakabaka1027 <alexanderdcha@gmail.com>
2017-04-18 03:36:17 -0400

    added main menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject introDoor;
+    public GameObject mainMenu;
+    public GameObject emitter;
+    public GameObject laser;
+
+
+
+	// Use this for initialization
+	void Start () {
+		laser.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BeginAscent() {
+        StartCoroutine(BeginAscentCoroutine());
+    }
+
+    IEnumerator BeginAscentCoroutine() {
+        yield return new WaitForSeconds(.5f);
+
+        mainMenu.GetComponent<Animator>().Play("Deactivate");
+
+        yield return new WaitForSeconds(1);
+        StartCoroutine(introDoor.GetComponent<Door>().OpenDoor());
+        
+        yield return new WaitForSeconds(4);
+
+              
+        emitter.GetComponent<Animator>().Play("Activate");
+
+        yield return new WaitForSeconds(3f);
+        laser.SetActive(true);
+
+
+
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+}
