602cc54f3490067272f4b550ec6ba181357fd25e
chakabaka1027 <alexanderdcha@gmail.com>
2017-04-18 16:59:14 -0400

    added typing effect to main menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class Menu : MonoBehaviour {
 
@@ -10,11 +12,16 @@ public class Menu : MonoBehaviour {
     public GameObject emitter;
     public GameObject laser;
 
-
+    public GameObject textComp;
+	public AudioClip putt;
+    public float startDelay = 2f;
+	public float typeDelay = 0.01f;
 
 	// Use this for initialization
 	void Start () {
 		laser.SetActive(false);
+        textComp.GetComponent<Text>().text = "";
+        StartCoroutine(TypeIn("The Cave "));
 	}
 	
 	// Update is called once per frame
@@ -55,5 +62,14 @@ public class Menu : MonoBehaviour {
         Application.Quit();
 
     }
+
+    public IEnumerator TypeIn(string msg){
+		yield return new WaitForSeconds(startDelay);
+		for (int i = 0; i < msg.Length; i++){
+			textComp.GetComponent<Text>().text = msg.Substring(0, i);
+			GetComponent<AudioSource>().PlayOneShot(putt, 0.03f);
+			yield return new WaitForSeconds(typeDelay);
+		}
+	}
 
 }
