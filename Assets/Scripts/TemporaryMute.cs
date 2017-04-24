a09f84151d2dd60e044f878523e23ab6b15a0fb5
chakabaka1027 <alexanderdcha@gmail.com>
2017-04-24 04:58:51 -0400

    rebuilt game, audiolistener's volume is now set to zero in opening moments of each level instead of disabled to avoid errors

--- Assets/Scripts/TemporaryMute.cs
+++ Assets/Scripts/TemporaryMute.cs
@@ -9,13 +9,14 @@ public class TemporaryMute : MonoBehaviour {
     private void Awake() {
 
         audioListener = GetComponent<AudioListener>();
-
+        AudioListener.volume = 0;
         StartCoroutine(MuteInitialAudio());
     }
 
     //remove audio for the beginning moments of the game
     IEnumerator MuteInitialAudio() {
         yield return new WaitForSeconds(2);
+        AudioListener.volume = 1;
 
         audioListener.enabled = true;
     }
